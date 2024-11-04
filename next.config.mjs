29e0cebabbcb4815542f4ca06a7dad4c4d5314f9
gr22nist <gr22nist@gmail.com>
2024-11-04 18:56:44 +0900

    🔧 설정 파일 업데이트

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const NextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default NextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: process.env.NODE_ENV !== 'development', // 개발 환경에서만 StrictMode 비활성화
+  reactStrictMode: process.env.NODE_ENV !== 'development',
 };
 
 export default nextConfig;
